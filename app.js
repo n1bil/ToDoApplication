1440aca0fc57e2084f7011f976771a310f471c5d
n1bil <nabil.nadzor@gmail.com>
2024-02-03 23:50:49 +0100

    Fix bug

--- app.js
+++ app.js
@@ -7,24 +7,23 @@ import passport from "passport";
 import session from "express-session";
 import { currentDateMiddleware } from "./utils/currentDateMiddleware.js";
 
-
 const app = express();
 const port = process.env.PORT || 3000;
 
 mongoose.set('strictQuery', false);
+
 const connectDB = async () => {
     try {
-        const connect = mongoose.connect(process.env.MONGODB_URI, {
+        await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-          console.log(`MongoDB Connected: ${connect.connection.host}`);
+        console.log(`MongoDB Connected: ${mongoose.connection.host}`);
     } catch (error) {
-        console.log(error);
+        console.error('Error connecting to MongoDB:', error.message);
         process.exit(1);
     }
 }
-
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -34,17 +33,13 @@ app.use(session({ secret: 'Our little secret.', resave: false, saveUninitialized
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
-
 app.use(currentDateMiddleware);
 app.use("/", tasksRoutes);
-
 
 connectDB().then(() => {
     app.listen(port, () => {
         console.log(`Server running on port ${port}`);
     })
-})
+});
 
 // mongoose.connect('mongodb://127.0.0.1:27017/todoDB', { useNewUrlParser: true });
